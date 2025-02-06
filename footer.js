a6d2411309535fc6ba89e0a114ad39e58788b13a
GNPsychology <gulenayab.hallian@gmail.com>
2025-02-06 06:34:09 +0000

    update

--- footer.js
+++ footer.js
@@ -2,7 +2,7 @@
 // Hover effect for social icons
 document.querySelectorAll('.social-icon').forEach(icon => {
     icon.addEventListener('mouseenter', () => {
-      icon.style.backgroundColor = '#444';
+      icon.style.backgroundColor = 'rgba(113, 150, 216, 0.774)';
     });
   
     icon.addEventListener('mouseleave', () => {
